0020564c6f7b9a5b1a47cba49e5e1aecd6286a6a
MinseokGo <rhalstjr1999@naver.com>
2024-06-14 22:17:38 +0900

    feat: jwt parse

--- src/main/java/study/likelionbeweekly/week7/jwt/JwtService.java
+++ src/main/java/study/likelionbeweekly/week7/jwt/JwtService.java
@@ -2,18 +2,18 @@ package study.likelionbeweekly.week7.jwt;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Jwts.SIG;
+import jakarta.persistence.EntityNotFoundException;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.spec.SecretKeySpec;
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.likelionbeweekly.week7.member.Member;
+import study.likelionbeweekly.week7.member.MemberRepository;
 
-@Slf4j
-@Setter
 @Service
-@ConfigurationProperties(prefix = "jwt")
+@RequiredArgsConstructor
 public class JwtService {
 
     private static final String TOKEN_PREFIX = "Bearer ";
@@ -25,7 +25,8 @@ public class JwtService {
     private static final Long MINUTES = 60L;
     private static final Long HOURS = 24L;
 
-    private String secretKey;
+    private final JwtProperties jwtProperties;
+    private final MemberRepository memberRepository;
 
     public String create(String email) {
         return TOKEN_PREFIX + Jwts.builder()
@@ -38,10 +39,30 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + MILLISECONDS * SECONDS * MINUTES * HOURS))
                 .signWith(
                         new SecretKeySpec(
-                                secretKey.getBytes(StandardCharsets.UTF_8),
+                                jwtProperties.getSecretKey()
+                                        .getBytes(StandardCharsets.UTF_8),
                                 SIG.HS256.key()
                                         .build()
                                         .getAlgorithm()))
                 .compact();
+    }
+
+    @Transactional(readOnly = true)
+    public Member parse(String token) {
+        String email = getEmail(token);
+        return memberRepository.findByEmail(email)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    private String getEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(
+                        SIG.HS256.key()
+                                .build()
+                )
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("email", String.class);
     }
 }
