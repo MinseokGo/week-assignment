85528ae11af8cac486cd704e1dfbcf394ac9f744
MinseokGo <rhalstjr1999@naver.com>
2024-06-14 20:53:38 +0900

    feat: enum constant

---
+++ src/main/java/study/likelionbeweekly/week7/utils/Constant.java
@@ -0,0 +1,11 @@
+package study.likelionbeweekly.week7.utils;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Constant {
+
+    AUTHORIZATION_HEADER_KEY("Authorization");
+
+    private final String content;
+}
