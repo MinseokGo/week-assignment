28fb7cd1bc37ccb1e20ba444def3229a967ba47f
MinseokGo <rhalstjr1999@naver.com>
2024-06-21 14:16:04 +0900

    refactor: add transaction in user detail service

--- src/main/java/study/likelionbeweekly/week7/security/jwt/CustomUserDetailsService.java
+++ src/main/java/study/likelionbeweekly/week7/security/jwt/CustomUserDetailsService.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import study.likelionbeweekly.week7.member.Member;
 import study.likelionbeweekly.week7.member.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
