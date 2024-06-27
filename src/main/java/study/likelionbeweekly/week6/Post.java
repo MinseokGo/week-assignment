e6f3ce4bdb64000e2c395113f7f1bce0d56b6d16
MinseokGo <rhalstjr1999@naver.com>
2024-06-27 16:12:31 +0900

    feat: post

---
+++ src/main/java/study/likelionbeweekly/week6/Post.java
@@ -0,0 +1,42 @@
+package study.likelionbeweekly.week6;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+public class Post {
+
+    private static final String DEFAULT_BOOLEAN = "boolean default false";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 63, nullable = false)
+    private String title;
+
+    @Column(length = 511, nullable = false)
+    private String content;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Member member;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column(columnDefinition = DEFAULT_BOOLEAN)
+    private boolean isDeleted;
+}
